a187f73c8312962517d5cd938f18c761998f9299
yhwu912 <yhwu912@gmail.com>
2017-06-19 15:39:37 +0800

    Scenario 2 紅燈

--- BookShoppingCart/BookShoppingCartTests/ShoppingCartCalculatorTests.cs
+++ BookShoppingCart/BookShoppingCartTests/ShoppingCartCalculatorTests.cs
@@ -26,5 +26,23 @@ namespace BookShoppingCart.Tests
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
+        [TestMethod()]
+        public void 第一集買了一本_第二集也買了一本_價格應為190元()
+        {
+            //Arrage
+            var books = new List<Book>
+            {
+                new Book { Id = Guid.NewGuid(), Name = "哈利波特第一集", Price = 100 },
+                new Book { Id = Guid.NewGuid(), Name = "哈利波特第二集", Price = 100 }
+            };
+
+            //Act
+            var actual = new ShoppingCartCalculator().Calculate(books);
+            decimal expected = 190;
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
